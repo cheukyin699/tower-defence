b59a9bde954a7f4bb3363c308deb4873f2e0e74d
cheukyin699 <cheukyin699@yahoo.com>
2015-04-21 19:52:29 -0700

    Game states skeleton established
    Kind-of

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Mode:
     '''
     Just some normal game modes
     '''
+    errormsg = -1
     splash = 0
     menu = 1
     playing = 2
@@ -23,21 +24,70 @@ class Color:
     green =     (  0, 255,   0)
     blue =      (  0,   0, 255)
 
+class State:
+    '''
+    The base class for all game states
+    '''
+    def __init__(self, surface, rmanager, state=Mode.splash):
+        self.surface = surface
+        self.rmanager = rmanager
+        self.state = state
+
+        self.SIZE = (surface.get_rect().w, surface.get_rect().h)
+
+class ErrorState(State):
+    '''
+    The class for handling unexpected errors.
+    Expect use in debugging ONLY.
+    (Or maybe if the sky is falling, then I'll consider.)
+    '''
+    def __init__(self, surface, rmanager, mesg):
+        State.__init__(self, surface, rmanager, mesg, state=Mode.errormsg)
+
+        self.mesg = mesg
+
+    def draw(self):
+        self.surface.fill(Color.black)
+
+        # Display the error message in RED
+        stat = self.rmanager.fonts['monospace'].render(self.mesg, True, Color.red)
+        statrect = stat.get_rect()
+        # ... in the middle of the screen
+        self.surface.blit(stat, (self.SIZE[0]/2-statrect.w/2, self.SIZE[1]/2-statrect.h/2))
+
+class SplashState(State):
+    '''
+    The class for handling the splash state
+    '''
+    def __init__(self, surface, rmanager):
+        State.__init__(self, surface, rmanager, state=Mode.splash)
+
+    def draw(self):
+        self.surface.fill(Color.black)
+
+        # Display the status text
+        stat = self.rmanager.fonts['monospace'].render(self.rmanager.status_text, True, Color.white)
+        statrect = stat.get_rect()
+        # ... in the middle of the screen
+        self.surface.blit(stat, (self.SIZE[0]/2-statrect.w/2, self.SIZE[1]/2-statrect.h/2))
+
+    def update(self):
+        '''
+        Updates the state of this guy.
+        Probably won't be used at all since 2 lines of code
+        is fairly easy to understand in a <10 line loop.
+        '''
+        if self.rmanager.loaded:
+            self.state = Mode.menu
 
 
-# FUNCTIONS
-def displaysplash(surface, rmanager, SIZE=(500,500)):
+class MenuState(State):
     '''
-    Displays the splash screen
+    The class for handling the menu state
     '''
-    # Display the statuses
-    stat = rmanager.fonts['monospace'].render(rmanager.status_text, True, Color.white)
-    statrect = stat.get_rect()
-    # Blit into middle of screen
-    surface.blit(stat, (SIZE[0]/2-statrect.w/2, SIZE[1]/2-statrect.h/2))
+    def __init__(self, surface, rmanager):
+        State.__init__(self, surface, rmanager, state=Mode.menu)
 
-def displaymenu(surface, rmanager, SIZE=(500,500)):
-    '''
-    Displays the main menu
-    '''
-    pass
+    def draw(self):
+        self.surface.fill(Color.black)
+
--- td.py
+++ td.py
@@ -23,6 +23,7 @@ rmanager = res_man.Resources()
 surface = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 mode = game.Mode.splash
+states = []
 
 
 
@@ -33,6 +34,8 @@ pygame.display.set_caption('Tower Defence')
 
 rmanager.loadFromJson('res/resources.json')
 rmanager.loadFonts()
+
+states.append(game.SplashState(surface, rmanager))
 
 
 # Set up multithreading
@@ -52,37 +55,53 @@ loadstuff = threading.Thread(target=managerinitwork)
 loadstuff.start()
 
 
-pygame.mixer.music.load(rmanager.musics['menu'])
-pygame.mixer.music.play(-1)
-
 
 # The loading splash screen
 while mode == game.Mode.splash:
-    surface.fill(game.Color.black)
-
     # Checks if it is still loading stuff
     if rmanager.loaded:
         mode = game.Mode.menu
 
-    game.displaysplash(surface, rmanager, SIZE=SIZE)
+    # Draw it!
+    states[-1].draw()
 
     clock.tick(60)
     pygame.display.flip()
 
+# Now that everything has been loaded successfully,
+# play some music for the menu.
+pygame.mixer.music.load(rmanager.musics['menu'])
+pygame.mixer.music.play(-1)
+
+
+# Remove the state off the stack
+# Add the menu state on the stack
+states.pop()
+states.append(game.MenuState(surface, rmanager))
 
 # The main loop
 while mode != game.Mode.exiting:
-    surface.fill(game.Color.black)
+    # VERY IMPORTANT: If the game state changes in class,
+    # but not outside, queue the state for deletion
+    # and delete it.
+    if states[-1].state != mode:
+        # TURN THEM
+        mode = states[-1].state
+
+        # Checks them ONE BY ONE
+        if states[-1].state == game.Mode.menu:
+            states.append(game.MenuState(surface, rmanager))
+
+        # Remove the LONER (safety issue)
+        if len(states) > 1:
+            states.pop(0)
+
+
     for evt in pygame.event.get():
         if evt.type == QUIT:
             mode = game.Mode.exiting
 
-    # For the different modes, draw different
-    # things on the screen (obviously)
-    if mode == game.Mode.menu:
-        # Draw the menu screen thingy
-        game.displaymenu(surface, rmanager, SIZE=SIZE)
-
+    states[-1].draw()
 
     clock.tick(60)
     pygame.display.flip()
