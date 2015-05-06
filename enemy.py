387d54f3d3874d900f88d7edb790db66f471607f
Ng Cheuk Yin <cheukyin699@yahoo.com>
2015-05-06 15:19:53 -0700

    Added HP bar

--- enemy.py
+++ enemy.py
@@ -14,10 +14,16 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.centery = 200
 
         # Set hit-points
+        self.maxhp = hp
         self.hp = hp
 
     def update(self):
         if self.hp <= 0:
             self.kill()
         self.rect.centerx += 1
-        self.rect.centery = 200*math.sin(math.radians(self.rect.centerx%360))+200
+        self.rect.centery = 150*math.sin(math.radians(self.rect.centerx%360))+200
+
+        # Blit the green health bar
+        self.image = pygame.surface.Surface((30,30))
+        pygame.draw.circle(self.image, game.Color.white, (15,15), 15)
+        pygame.draw.rect(self.image, game.Color.green, (0,0,self.rect.w*self.hp/self.maxhp,5))
